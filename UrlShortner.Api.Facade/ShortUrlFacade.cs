f247b6cfc5c585a087a45cf88f5d7986048c9723
Bruno Silva <68924002+BrunoRafaSilva@users.noreply.github.com>
2025-03-25 21:39:12 -0300

    feat: enhance URL registration response with success message and data

--- UrlShortner.Api.Facade/ShortUrlFacade.cs
+++ UrlShortner.Api.Facade/ShortUrlFacade.cs
@@ -1,6 +1,7 @@
 
 using Microsoft.AspNetCore.Mvc;
 using UrlShortner.Api.Facade.Interfaces;
+using UrlShortner.Api.Models.ShortUrl.Responses;
 using UrlShortner.Api.Services.Database.Interfaces;
 using UrlShortner.Models;
 
@@ -24,7 +25,28 @@ namespace UrlShortner.Api.Facade
                 OwnerId = userId
             };
             var registerShortUrl = await _databaseService.RegisterShortUrlAsync(urlToShort);
-            return registerShortUrl;
+            if (registerShortUrl is ObjectResult objectResult && objectResult.Value is List<ShortUrls> value && value.Count > 0)
+            {
+                var shortUrl = value[0];
+                var response = new RegisterNewUrlResponse
+                {
+                    LinkReference = shortUrl.LinkReference ?? string.Empty,
+                    OriginalUrl = shortUrl.OriginalUrl ?? string.Empty,
+                };
+
+                return new OkObjectResult(new
+                {
+                    success = true,
+                    message = "Short URL registered successfully.",
+                    data = response
+                });
+            }
+
+            return new BadRequestObjectResult(new
+            {
+                success = false,
+                message = "Failed to register short URL."
+            });
         }
 
         public async Task<IActionResult> GetOriginalUrlAsync()
--- UrlShortner.Api.Models/ShortUrl/Responses/RegisterNewUrlResponse.cs
+++ UrlShortner.Api.Models/ShortUrl/Responses/RegisterNewUrlResponse.cs
@@ -1,14 +1,12 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace UrlShortner.Api.Models.ShortUrl.Responses
 {
     public class RegisterNewUrlResponse
     {
         [Required]
-        public required string Id { get; set; }
+        public required string LinkReference { get; set; }
+        [Required]
+        public required string OriginalUrl { get; set; }
     }
 }
